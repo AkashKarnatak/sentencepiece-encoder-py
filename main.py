e4ad6ffec59a2a3b9593c84d907b45038e1863ab
AkashKarnatak <akashkarnatak.ak47@gmail.com>
2024-08-26 14:51:58 +0530

    Add ids as well

--- main.py
+++ main.py
@@ -2,27 +2,27 @@ import sentencepiece_model_pb2 as spm
 
 model = spm.ModelProto()
 
-with open('../xlnet-base-cased-spiece.model', 'rb') as f:
+with open("../xlnet-base-cased-spiece.model", "rb") as f:
     model.ParseFromString(f.read())
 
-s = 'today is a great day'
-s = '▁' + s.replace(' ', '▁')
+s = "today is a great day"
+s = "▁" + s.replace(" ", "▁")
 
-scores = [float('inf')] * len(s)
+scores = [float("inf")] * len(s)
 ranges = [(i, i) for i in range(len(s))]
 
-vocab = {x.piece: x.score for x in model.pieces}
+vocab = {x.piece: {"score": x.score, "id": i} for i, x in enumerate(model.pieces)}
 
 # viterbi forward
 for eos in range(len(s)):
     for bos in range(eos + 1):
-        substr = s[bos:eos+1]
+        substr = s[bos : eos + 1]
         if substr not in vocab:
             continue
 
-        curr = -vocab[substr]
+        curr = -vocab[substr]['score']
         if bos > 0:
-            curr += scores[bos-1]
+            curr += scores[bos - 1]
         if curr < scores[eos]:
             scores[eos] = curr
             ranges[eos] = (bos, eos)
@@ -32,8 +32,9 @@ best_split = []
 i = len(s) - 1
 while i >= 0:
     bos, eos = ranges[i]
-    best_split.append(s[bos: eos+1])
-    i = bos-1
+    best_split.append(s[bos : eos + 1])
+    i = bos - 1
 best_split.reverse()
 
 print(best_split)
+print([vocab[x]['id'] for x in best_split])
